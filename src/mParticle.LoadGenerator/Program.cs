8998a5b2f8ec23846f91cc98e133576e292976d4
Andre Machado <andre.machado@nymity.com>
2021-05-24 13:54:52 -0400

    add initial version of solution

--- src/mParticle.LoadGenerator/Program.cs
+++ src/mParticle.LoadGenerator/Program.cs
@@ -1,25 +1,91 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using mParticle.LoadGenerator.Services;
 
 namespace mParticle.LoadGenerator
 {
     public sealed class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            string configFile = "config.json";
+            var configFile = "config.json";
             if (args.Length > 0)
             {
                 configFile = args[0];
             }
 
-            Config config = Config.GetArguments(configFile);
+            var config = Config.GetArguments(configFile);
             if (config == null)
             {
                 Console.WriteLine("Failed to parse configuration.");
                 return;
             }
 
-            // TODO Do some work!
+            await RunLoad(config);
+        }
+
+        private static async Task RunLoad(Config config)
+        {
+            var apiClient = new ApiClient(config, new HttpClient());
+            var currentRps = 1;
+            var currentPower = 1;
+
+            LogStatus(currentRps, config.TargetRPS);
+
+            while (true)
+            {
+                var numberOfTasks = config.SecondsPerRate * currentRps;
+
+                var tasks = CreateTasks(
+                    numberOfTasks,
+                    () => apiClient.CallApiEndpointAsync().Result);
+
+                foreach (var task in tasks)
+                {
+                    try
+                    {
+                        task.Start();
+                        await task;
+
+                        Console.Write(".");
+                        await Task.Delay(1000 / currentRps);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine();
+                        Console.Write($"Error - ${e.Message}");
+                    }
+                }
+
+                currentPower++;
+                currentRps = Math.Min((int)config.TargetRPS, (int)Math.Pow(2, currentPower));
+
+                if (currentRps > config.TargetRPS)
+                {
+                    return;
+                }
+
+                Console.WriteLine();
+                LogStatus(currentRps, config.TargetRPS);
+            }
+        }
+
+        private static IEnumerable<Task<T>> CreateTasks<T>(int numberOfTasks, Func<T> func)
+        {
+            var tasks = new List<Task<T>>();
+            for (var i = 0; i < numberOfTasks; i++)
+            {
+                tasks.Add(new Task<T>(func));
+            }
+
+            return tasks;
+        }
+
+        private static void LogStatus(int currentRps, uint targetRps)
+        {
+            Console.WriteLine($"Current RPS: {currentRps}\t\t Target RPS: {targetRps}");
         }
     }
 }
---
+++ src/mParticle.LoadGenerator/Services/ApiClient.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using mParticle.LoadGenerator.Models;
+using Newtonsoft.Json;
+
+namespace mParticle.LoadGenerator.Services
+{
+    public class ApiClient
+    {
+        private readonly Config config;
+
+        private readonly HttpClient client;
+
+        private readonly string postPath;
+
+        public ApiClient(Config config, HttpClient client)
+        {
+            this.config = config;
+            this.client = client;
+
+            var serverUri = new Uri(this.config.ServerURL);
+            this.client.BaseAddress = new Uri($"{serverUri.Scheme}://{serverUri.Host}");
+            this.client.DefaultRequestHeaders.Add("X-Api-Key", this.config.AuthKey);
+            this.postPath = serverUri.AbsolutePath;
+        }
+
+        public async Task<MyRequestResponse> CallApiEndpointAsync()
+        {
+            var myRequest = new MyRequest { Name = this.config.UserName };
+
+            var requestBody = new StringContent(JsonConvert.SerializeObject(myRequest));
+            
+            try
+            {
+                //var response = await this.client.PostAsync(this.postPath, requestBody);
+                //response.EnsureSuccessStatusCode();
+                //return JsonConvert.DeserializeObject<MyRequestResponse>(await response.Content.ReadAsStringAsync());
+                return await Task.FromResult(new MyRequestResponse() { Successful = true });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
