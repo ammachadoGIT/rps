34bbed2aa67a5b8de420cd385acd79b5a09cd4aa
Andre Machado <andre.machado@nymity.com>
2021-05-24 13:54:19 -0400

    add models

---
+++ src/mParticle.LoadGenerator/Models/MyRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace mParticle.LoadGenerator.Models
+{
+    public class MyRequest
+    {
+        public string Name { get; set; }
+        
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+
+        [JsonPropertyName("requests_sent")]
+        public uint RequestsSent { get; set; }
+    }
+}
---
+++ src/mParticle.LoadGenerator/Models/MyRequestResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace mParticle.LoadGenerator.Models
+{
+    public class MyRequestResponse
+    {
+        public bool Successful { get; set; }
+    }
+}
