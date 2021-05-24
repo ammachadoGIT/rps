7f0c50a6efe790a9097518d80ae85776d648b362
Andre Machado <andre.machado@nymity.com>
2021-05-24 14:17:07 -0400

    improve output of tasks, fix control whenn target rps is reached

--- src/mParticle.LoadGenerator/Program.cs
+++ src/mParticle.LoadGenerator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using mParticle.LoadGenerator.Services;
@@ -28,6 +29,7 @@ namespace mParticle.LoadGenerator
 
         private static async Task RunLoad(Config config)
         {
+            var targetRpsReached = false;
             var apiClient = new ApiClient(config, new HttpClient());
             var currentRps = 1;
             var currentPower = 1;
@@ -47,6 +49,7 @@ namespace mParticle.LoadGenerator
                     try
                     {
                         task.Start();
+                        Console.WriteLine($"task {task.Id} started");
                         await Task.Delay(1000 / currentRps);
                     }
                     catch (Exception e)
@@ -59,14 +62,37 @@ namespace mParticle.LoadGenerator
                 currentPower++;
                 currentRps = Math.Min((int)config.TargetRPS, (int)Math.Pow(2, currentPower));
 
-                if (currentRps > config.TargetRPS)
+                if (IsEndOfExecution(config, currentRps, targetRpsReached))
                 {
                     return;
                 }
 
+                if (HasReachedTargetRps(config, currentRps))
+                {
+                    targetRpsReached = true;
+                }
+
+                await Task.WhenAll(tasks);
+
+                var successfulTasksCount = tasks.Count(task => task.Status == TaskStatus.RanToCompletion);
+                Console.WriteLine($"Tasks executed successfully: {successfulTasksCount}");
+
+                var failedTasks = tasks.Where(task => task.Status == TaskStatus.Faulted);
+                Console.WriteLine($"Tasks failed: {failedTasks.Count()}");
+
                 Console.WriteLine();
                 LogStatus(currentRps, config.TargetRPS);
             }
+        }
+
+        private static bool HasReachedTargetRps(Config config, int currentRps)
+        {
+            return currentRps == config.TargetRPS;
+        }
+
+        private static bool IsEndOfExecution(Config config, int currentRps, bool targetRpsReached)
+        {
+            return HasReachedTargetRps(config, currentRps) && targetRpsReached;
         }
 
         private static IEnumerable<Task<T>> CreateTasks<T>(int numberOfTasks, Func<T> func)
