17fbda3aa5eaa6d9d762c56a25b7f189c670b5d9
Andre Machado <andre.machado@nymity.com>
2021-05-24 21:42:12 -0400

    fix code by canceling tasks after a second

--- src/mParticle.LoadGenerator/Program.cs
+++ src/mParticle.LoadGenerator/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using mParticle.LoadGenerator.Services;
 
@@ -29,88 +30,46 @@ namespace mParticle.LoadGenerator
 
         private static async Task RunLoad(Config config)
         {
-            var targetRpsReached = false;
             var apiClient = new ApiClient(config, new HttpClient());
-            var currentRps = 1;
-            var currentPower = 0;
-
-            LogStatus(currentRps, config.TargetRPS);
-
+            
             while (true)
             {
-                var numberOfTasks = config.SecondsPerRate * currentRps;
+                var tokenSource = new CancellationTokenSource();
 
                 var tasks = CreateTasks(
-                    numberOfTasks,
-                    () => apiClient.CallApiEndpointAsync().Result);
+                    config.TargetRPS,
+                    () => apiClient.CallApiEndpointAsync(tokenSource.Token).Result);
 
-                foreach (var task in tasks)
-                {
-                    task.Start();
-                    Console.WriteLine($"task {task.Id} started");
-                    await Task.Delay(1000 / currentRps);
-                }
-
-                currentPower++;
-                currentRps = Math.Min((int)config.TargetRPS, (int)Math.Pow(2, currentPower));
-
-                if (IsEndOfExecution(config, currentRps, targetRpsReached))
-                {
-                    return;
-                }
-
-                if (HasReachedTargetRps(config, currentRps))
-                {
-                    targetRpsReached = true;
-                }
+                tokenSource.CancelAfter(1000);
 
                 try
                 {
                     await Task.WhenAll(tasks);
                 }
-                catch (AggregateException e)
+                catch (Exception)
                 {
-                    foreach (var ex in e.InnerExceptions)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
+                    // ignored
                 }
 
-                var successfulTasksCount = tasks.Count(task => task.Status == TaskStatus.RanToCompletion);
-                Console.WriteLine($"Tasks executed successfully: {successfulTasksCount}");
+                var succesfulTasks = tasks.Count(task => task.Status == TaskStatus.RanToCompletion);
 
-                var failedTasks = tasks.Where(task => task.Status == TaskStatus.Faulted);
-                Console.WriteLine($"Tasks failed: {failedTasks.Count()}");
-
-                Console.WriteLine();
-                LogStatus(currentRps, config.TargetRPS);
+                var rps = (float)succesfulTasks / tasks.Count() * 100;
+                Console.WriteLine($"Current RPS: {rps}");
             }
         }
 
-        private static bool HasReachedTargetRps(Config config, int currentRps)
-        {
-            return currentRps == config.TargetRPS;
-        }
-
-        private static bool IsEndOfExecution(Config config, int currentRps, bool targetRpsReached)
-        {
-            return HasReachedTargetRps(config, currentRps) && targetRpsReached;
-        }
-
-        private static IEnumerable<Task<T>> CreateTasks<T>(int numberOfTasks, Func<T> func)
+        private static IEnumerable<Task<T>> CreateTasks<T>(uint numberOfTasks, Func<T> func)
         {
             var tasks = new List<Task<T>>();
+
             for (var i = 0; i < numberOfTasks; i++)
             {
-                tasks.Add(new Task<T>(func));
+                var newTask = new Task<T>(func);
+                newTask.Start();
+                tasks.Add(newTask);
             }
-
+            
             return tasks;
         }
-
-        private static void LogStatus(int currentRps, uint targetRps)
-        {
-            Console.WriteLine($"Current RPS: {currentRps}\t\t Target RPS: {targetRps}");
-        }
     }
-}
+    }
